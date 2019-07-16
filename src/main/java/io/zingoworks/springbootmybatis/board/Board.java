a41fdc016838e7661dad760dedfc475a373be815
JINSAN NOH <zingoworks@gmail.com>
2019-07-16 10:10:00 +0900

    Apply lombok library

--- src/main/java/io/zingoworks/springbootmybatis/board/Board.java
+++ src/main/java/io/zingoworks/springbootmybatis/board/Board.java
@@ -1,10 +1,20 @@
 package io.zingoworks.springbootmybatis.board;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Board {
 
     private long id;
     private String title;
     private String content;
+
+    //TODO
+    // default 생성자 필요한 경우와 이유
 
     public Board(String title, String content) {
         this.title = title;
@@ -20,40 +30,6 @@ public class Board {
     public void update(Board board) {
         this.title = board.title;
         this.content = board.content;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Board{");
-        sb.append("id=").append(id);
-        sb.append(", title='").append(title).append('\'');
-        sb.append(", content='").append(content).append('\'');
-        sb.append('}');
-        return sb.toString();
     }
 
 }
