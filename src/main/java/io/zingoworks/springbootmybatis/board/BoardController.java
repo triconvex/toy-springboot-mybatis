bfa745c6a5559affe7b66e294560690dee43eec8
JINSAN NOH <zingoworks@gmail.com>
2019-07-16 17:36:52 +0900

    Change swagger log level setting

--- src/main/java/io/zingoworks/springbootmybatis/board/BoardController.java
+++ src/main/java/io/zingoworks/springbootmybatis/board/BoardController.java
@@ -1,6 +1,7 @@
 package io.zingoworks.springbootmybatis.board;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.zingoworks.springbootmybatis.response.ApiResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,27 +15,37 @@ public class BoardController {
 
     @ApiOperation(value = "게시물 작성")
     @PostMapping
-    public ApiResult<String> create(@RequestBody BoardRequest boardRequest) {
+    public ApiResult<String> create(
+            @ApiParam(value = "작성 게시물 제목과 내용")
+            @RequestBody BoardRequest boardRequest) {
         boardService.create(boardRequest.toBoard());
         return new ApiResult<>("게시물 작성 성공");
     }
 
     @ApiOperation(value = "게시물 조회")
     @GetMapping("/{id}")
-    public ApiResult<Board> read(@PathVariable long id) {
+    public ApiResult<Board> read(
+            @ApiParam(value = "조회하는 게시물의 id")
+            @PathVariable long id) {
         return new ApiResult<>(boardService.findById(id));
     }
 
 
     @ApiOperation(value = "게시물 수정")
     @PutMapping("/{id}")
-    public ApiResult<Board> update(@PathVariable long id, @RequestBody BoardRequest boardRequest) {
+    public ApiResult<Board> update(
+            @ApiParam(value = "수정하려는 게시물의 id")
+            @PathVariable long id,
+            @ApiParam(value = "수정 된 제목과 내용")
+            @RequestBody BoardRequest boardRequest) {
         return new ApiResult<>(boardService.update(id, boardRequest.toBoard()));
     }
 
     @ApiOperation(value = "게시물 삭제")
     @DeleteMapping("/{id}")
-    public ApiResult<String> delete(@PathVariable long id) {
+    public ApiResult<String> delete(
+            @ApiParam(value = "삭제하려는 게시물의 id")
+            @PathVariable long id) {
         boardService.delete(id);
         return new ApiResult<>("게시물 삭제 성공");
     }
