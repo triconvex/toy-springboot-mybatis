1db58e74d58e8c47b7dad0c15a89c2d4569f915d
JINSAN NOH <zingoworks@gmail.com>
2019-07-16 17:30:04 +0900

    Add swagger2 dependencies with configuration

--- src/main/java/io/zingoworks/springbootmybatis/board/BoardController.java
+++ src/main/java/io/zingoworks/springbootmybatis/board/BoardController.java
@@ -1,5 +1,6 @@
 package io.zingoworks.springbootmybatis.board;
 
+import io.swagger.annotations.ApiOperation;
 import io.zingoworks.springbootmybatis.response.ApiResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,22 +12,27 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    @ApiOperation(value = "게시물 작성")
     @PostMapping
     public ApiResult<String> create(@RequestBody BoardRequest boardRequest) {
         boardService.create(boardRequest.toBoard());
         return new ApiResult<>("게시물 작성 성공");
     }
 
+    @ApiOperation(value = "게시물 조회")
     @GetMapping("/{id}")
     public ApiResult<Board> read(@PathVariable long id) {
         return new ApiResult<>(boardService.findById(id));
     }
 
+
+    @ApiOperation(value = "게시물 수정")
     @PutMapping("/{id}")
     public ApiResult<Board> update(@PathVariable long id, @RequestBody BoardRequest boardRequest) {
         return new ApiResult<>(boardService.update(id, boardRequest.toBoard()));
     }
 
+    @ApiOperation(value = "게시물 삭제")
     @DeleteMapping("/{id}")
     public ApiResult<String> delete(@PathVariable long id) {
         boardService.delete(id);
